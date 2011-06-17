1963fe93872b25d7d60b1be23e7db9fc674e7318
Gøran Hansen <mail@goeran.no>
2011-06-17 11:32:10 +0200

    pair programmign in dojo @ statoil

--- tests/workstation.spec.js
+++ tests/workstation.spec.js
@@ -12,6 +12,23 @@ describe("script", function() {
 	});
 });
 
+describe("abstract syntax tree", function() {
+	it("should throw exception when index is out of range", function() {
+		screen("screen 1", function() {
+			label("my label");
+		});
+		
+		expect(getWidget(-1)).toThrow("Index out of range.");
+		expect(getWidget(100)).toThrow("Index out of range.");
+	});
+	
+	function getWidget(index) {
+		return function() {
+			workstation.lastScreen().getWidget(index);
+		}
+	}  
+});
+
 describe("keywords", function() {
 	beforeEach(function() {
 		workstation.ast = [];
@@ -61,17 +78,17 @@ describe("keywords", function() {
 				
 			});
 			
-			expect(lastScreenInAst().views.length).toEqual(1);
-			expect(lastScreenInAst().views[0].type).toEqual("label");
+			expect(lastScreenInAst().numberOfWidgets()).toEqual(1);
+			expect(lastScreenInAst().getWidget(0).type).toEqual("label");
 		});
 		
 		it("should set properties", function() {
 			screen("screen 1", function() {
 				label("hello world");
 				
-				expect(lastScreenInAst().views[0].type).toEqual("label");
-				expect(lastScreenInAst().views[0].text).toEqual("hello world");
-				expect(lastScreenInAst().views[0].style).toBeDefined();
+				expect(lastScreenInAst().getWidget(0).type).toEqual("label");
+				expect(lastScreenInAst().getWidget(0).text).toEqual("hello world");
+				expect(lastScreenInAst().getWidget(0).style).toBeDefined();
 			});	
 		});
 		
@@ -83,12 +100,13 @@ describe("keywords", function() {
 	describe("textbox", function() {
 		it("should be appended to the last screen in the AST", function() {
 			screen("screen 1", function() {
-				textbox("Username", function() {
-				});
+				textbox("Username");
+				textbox();
 			});
 			
-			expect(lastScreenInAst().views.length).toEqual(1);
-			expect(lastScreenInAst().views[0].type).toEqual("textbox");
+			expect(lastScreenInAst().numberOfWidgets()).toEqual(2);
+			expect(lastScreenInAst().getWidget(0).type).toEqual("textbox");
+			expect(lastScreenInAst().getWidget(1).type).toEqual("textbox");
 		})
 	});
 });
